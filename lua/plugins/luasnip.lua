fe0218b8bd430e8bc85261ddf06823fc78119427
Oguro Luke <luke.oguro@student-cs.fr>
2025-01-11 00:43:23 +0100

    set luasnip

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,5 +1,8 @@
 return {
   'L3MON4D3/LuaSnip',
   version = "v2.3.0",
-  run = "make install_jsregexp"
+  run = "make install_jsregexp",
+  config = function()
+    require("luasnip.loaders.from_lua").load()
+  end,
 }
