6cebf4f2e178d7c960b4c321b36acf58948965cb
Luke Oguro <eku1gyps0phi1a@gmail.com>
2026-01-26 21:50:12 +0100

    change python lsp server

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,12 +1,42 @@
 return {
   'neovim/nvim-lspconfig',
   ft = {
+    "c",
+    "cpp",
     "python",
     "rust",
   },
   config = function()
-    require'lspconfig'.pylsp.setup{}
-    require'lspconfig'.rust_analyzer.setup{
+    vim.lsp.config('basedpyright', {
+      basedpyright = {
+	analysis = {
+	  typeCheckingMode = "standard",
+	  autoSearchPaths = true,
+	  useLibraryCodeForTypes = true,
+	  diagnosticMode = "openFilesOnly",
+	},
+      },
+    })
+    vim.lsp.enable('basedpyright')
+
+    vim.lsp.config('ruff', {
+      capabilities = {
+	general = {positionEncodings = {"utf-16"}},
+      },
+      on_attach = function(client, bufnr)
+	client.server_capabilities.hoverProvider = false
+      end,
+    })
+    vim.lsp.enable('ruff')
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*.py",
+      callback = function()
+	vim.lsp.buf.format({async = false})
+	vim.cmd("LspRestart ruff")
+      end,
+    })
+    
+    vim.lsp.config('rust_analyzer', {
       on_attach = function(client, bufnr)
 	vim.lsp.inlay_hint.enable(false, { bufnr = bufnr })
       end,
@@ -28,7 +58,10 @@ return {
 	  },
 	}
       }
-    }
-    require'lspconfig'.clangd.setup{}
+    })
+    vim.lsp.enable('rust_analyzer')
+
+    vim.lsp.config('clangd', {})
+    vim.lsp.enable('clangd')
   end,
 }
